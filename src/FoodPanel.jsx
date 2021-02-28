68a0ebffca54db4bcca5ebda145ca642e6846d4c
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2021-02-27 18:30:56 -0600

    codestyle tweak

--- src/FoodPanel.jsx
+++ src/FoodPanel.jsx
@@ -7,7 +7,8 @@ export default function FoodPanel({ food, buttons, small }) {
       <img className={small ? "w-24 h-24" : "w-32 h-32"} src={food.img} alt={food.name} />
       
       {buttons.map(btn => (
-        <button className={"border rounded-sm bg-gray-300 " + (small ? 'p-0' : 'p-1')} key={btn.text}
+        <button className={"border rounded-sm bg-gray-300 " + (small ? 'p-0' : 'p-1')}
+                key={btn.text}
                 onClick={btn.fn}>
           {btn.text}
         </button>
